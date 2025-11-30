f901d49c63e8eac0e01f9e35713e5f4860c6679b
Muhammad Irsyad Aulia <email-irsyadjokam313@gmail.com>
2025-11-30 11:48:01 +0700

    Feat: Add conditional redering logic to Counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -11,10 +11,16 @@ export default function Counter() {
         <div className="flex flex-col items-center mt-8 p-6 bg-white rounded-lg shadow-md">
             <h2 className="text-xl font-bold mb-4">Counter App</h2>
 
-            {/* Tampilkan nilai count */}
             <p className="text-4xl font-bold text-blue-600 mb-4">{count}</p>
             
-            {/* Tombol pemicu */}
+            {count >= 10 ? (
+                <p className="text-green-600 font-bold mb-4 animate-bounce">
+                    Wow! You clicked a lot! 🎉
+                </p>
+             ) : null}
+
+             {/* --------------------------------- */}
+             
             <button
             onClick={handleClick}
             className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition"
